4451664999f2375bd09f94cf721f76f786818edb
Pri_030 <priyankasingh30@gmail.com>
2020-04-18 18:13:27 -0400

    updated script file

--- script.js
+++ script.js
@@ -19,3 +19,50 @@ var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", ">", "<", "(", ")", "{", "}"];
+
+
+//adding a event listener on a button click
+getPasswordButton.addEventListener("click", function () {
+    //storing slider value in a variable
+    var passwordLength = document.querySelector("#selectRange").value;
+
+    //defining empty arrays
+    var optionSelected = [];
+    var password = [];
+
+    //checking different possibilities for checkboxes
+    if (upperCaseCheckbox.checked === false && lowerCaseCheckbox.checked === false && numberCheckbox.checked === false && specialCharCheckbox.checked === false) {
+        alert("You must select atleast one checkbox");
+        return;
+    }
+
+    if (specialCharCheckbox.checked === true) {
+        optionSelected = optionSelected.concat(specialChars);
+        console.log(optionSelected);
+    }
+
+
+    if (upperCaseCheckbox.checked === true) {
+        optionSelected = optionSelected.concat(upperCaseLetters);
+        console.log(optionSelected);
+    }
+    if (lowerCaseCheckbox.checked === true) {
+        optionSelected = optionSelected.concat(lowerCaseLetters);
+        console.log(optionSelected);
+    }
+    if (numberCheckbox.checked == true) {
+        optionSelected = optionSelected.concat(numbers);
+        console.log(optionSelected);
+    }
+
+    for (var i = 0; i < passwordLength; i++) {
+        //using  math function to generate random numbers
+        var randomValue = Math.floor(Math.random() * optionSelected.length);
+        var password = password + optionSelected[randomValue];
+
+        //stoing password in a textbox
+        document.querySelector("#txt").value = password;
+
+    }
+
+});
